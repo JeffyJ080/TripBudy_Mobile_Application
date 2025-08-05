9dc81bfc40ddde8a9d355b115e128ff320e4d278
Jeffy Jefferson <jeffyj080@gmail.com>
2025-08-05 22:51:16 +0200

    Spinner text contents extracted and displayed. Can be utilised in DB programming next

--- app/src/main/java/com/example/tripbudymobileapplication/ui/TripPlanningActivity.java
+++ app/src/main/java/com/example/tripbudymobileapplication/ui/TripPlanningActivity.java
@@ -2,9 +2,13 @@ package com.example.tripbudymobileapplication.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,8 +18,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.tripbudymobileapplication.R;
 
-public class TripPlanningActivity extends AppCompatActivity {
+public class TripPlanningActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    private String[] exCategories = {
+            "Food", "Entertainment",
+            "Travel"
+    };
+
+    private String exCat;
     private Button btnNext;
     private Spinner spnCategories;
 
@@ -32,12 +42,15 @@ public class TripPlanningActivity extends AppCompatActivity {
 
         // Spinner Code
         spnCategories = findViewById(R.id.spinner2);
+        spnCategories.setOnItemSelectedListener(this);
+
         //Array Adapter
-        ArrayAdapter <CharSequence> adapter = ArrayAdapter.createFromResource(
+        ArrayAdapter <String> adapter = new ArrayAdapter<>(
                 this,
-                R.array.expenseCategories,
-                android.R.layout.simple_spinner_item
+                android.R.layout.simple_spinner_item,
+                exCategories
         );
+
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spnCategories.setAdapter(adapter);
 
@@ -48,5 +61,22 @@ public class TripPlanningActivity extends AppCompatActivity {
             Intent intent = new Intent(TripPlanningActivity.this, BudgetingActivity.class);
             startActivity(intent);
         });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id){
+//        Toast.makeText(getApplicationContext(), exCategories[pos], Toast.LENGTH_SHORT).show();
+        exCat = exCategories[pos];
+        Toast.makeText(this, exCat, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent){
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
