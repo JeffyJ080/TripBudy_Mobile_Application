0b1575284f5396b4bc15c06392cf055dd56762c4
Jeffy Jefferson <jeffyj080@gmail.com>
2025-08-20 23:49:23 +0200

    Added user input to database using the user model Added user reading from database using the user model

--- app/src/main/java/com/example/tripbudymobileapplication/database/dao/UserDao.java
+++ app/src/main/java/com/example/tripbudymobileapplication/database/dao/UserDao.java
@@ -6,6 +6,10 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.example.tripbudymobileapplication.database.DatabaseHelper;
+import com.example.tripbudymobileapplication.database.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserDao {
 
@@ -24,7 +28,33 @@ public class UserDao {
         return db.insert("users", null, values);
     }
 
+    public User insertUser(User u){
+        ContentValues values = new ContentValues();
+        values.put("username", u.getUserName());
+        values.put("email", u.getUserEmail());
+        values.put("trips", u.getTotalTrips());
+        db.insert("users", null, values);
+
+        return u;
+    }
+
     public Cursor getAllUsers(){
         return db.query("users", null, null, null, null, null, null);
+    }
+
+    public List<User> getALlUsers(){
+        List<User> users = new ArrayList<>();
+        Cursor cursor = db.query("users", null, null, null, null, null, null);
+
+        while (cursor.moveToNext()){
+            String username = cursor.getString(cursor.getColumnIndexOrThrow("username"));
+            String email = cursor.getString(cursor.getColumnIndexOrThrow("email"));
+            Integer trips = cursor.getInt(cursor.getColumnIndexOrThrow("trips"));
+
+            users.add(new User(username, email, trips));
+        }
+        cursor.close();
+
+        return users;
     }
 }
