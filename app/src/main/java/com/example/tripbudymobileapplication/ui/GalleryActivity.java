56a74edbb05a16e9c71c81f685f1f48098c89e69
Jeffy Jefferson <jeffyj080@gmail.com>
2025-08-20 13:35:07 +0200

    Image displayed in grid based on the memory array. Error checking and fallback added for if the image was not found. started dynamically adding items.

--- app/src/main/java/com/example/tripbudymobileapplication/ui/GalleryActivity.java
+++ app/src/main/java/com/example/tripbudymobileapplication/ui/GalleryActivity.java
@@ -2,9 +2,14 @@ package com.example.tripbudymobileapplication.ui;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +18,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.tripbudymobileapplication.R;
+import com.example.tripbudymobileapplication.model.Memory;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class GalleryActivity extends AppCompatActivity {
 
     private ImageButton btnTrips, btnHome, btnAddMem, btnViewMemory, btnAccount;
+    private ArrayList<Memory> memories = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +77,32 @@ public class GalleryActivity extends AppCompatActivity {
             overridePendingTransition(0, 0);
         });
 
+        // Gallery Code
+        GridLayout grid = findViewById(R.id.glGallery);
 
+        for (Memory m :
+                memories) {
+            // Container for each memory
+            LinearLayout container = new LinearLayout(this);
+            container.setOrientation(LinearLayout.VERTICAL);
+            container.setGravity(Gravity.CENTER);
+            container.setPadding(8, 8, 8, 8);
+
+            // Imageview
+            ImageView view = new ImageView(this);
+            view.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+            // Load the image
+            File file = new File(getFilesDir(), m.getImgPath());
+            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+            if (bitmap != null){
+                view.setImageBitmap(bitmap);
+            } else{
+                view.setImageResource(R.drawable.placeholder_foreground);
+            }
+
+            // Caption textview
+
+        }
     }
 }
